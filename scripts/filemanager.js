98fe75bd6785ee5cc87d2c26531ddbfebe80ce9a
Eric Christiaanse <eric@eric-op-het.net>
2011-08-25 13:36:52 +0200

    Make file_exists() accessable public ($.file_exists) and pass language info to image-editor connector

--- scripts/filemanager.js
+++ scripts/filemanager.js
@@ -25,6 +25,31 @@
             return 0;
     };
 
+    // Test if a given url exists
+    $.file_exists = function (url) {
+        // http://kevin.vanzonneveld.net
+        // +   original by: Enrique Gonzalez
+        // +      input by: Jani Hartikainen
+        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // %        note 1: This function uses XmlHttpRequest and cannot retrieve resource from different domain.
+        // %        note 1: Synchronous so may lock up browser, mainly here for study purposes. 
+        // *     example 1: file_exists('http://kevin.vanzonneveld.net/pj_test_supportfile_1.htm');
+        // *     returns 1: '123'
+        var req = window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
+        if (!req) {
+            throw new Error('XMLHttpRequest not supported');
+        }
+
+        // HEAD Results are usually shorter (faster) than GET
+        req.open('HEAD', url, false);
+        req.send(null);
+        if (req.status == 200) {
+            return true;
+        }
+
+        return false;
+    };
+    
     /*---------------------------------------------------------
     Setup, Layout, and Status Functions
     ---------------------------------------------------------*/
@@ -36,7 +61,7 @@
 
     // Get localized messages from file 
     // through culture var or from URL
-    if ($.urlParam('langCode') != 0 && file_exists('scripts/languages/' + $.urlParam('langCode') + '.js')) culture = $.urlParam('langCode');
+    if ($.urlParam('langCode') != 0 && $.file_exists('scripts/languages/' + $.urlParam('langCode') + '.js')) culture = $.urlParam('langCode');
     var lg = [];
     $.ajax({
         url: 'scripts/languages/' + culture + '.js',
@@ -80,31 +105,6 @@
             $('#grid').removeClass('ON');
         }
     };
-
-    // Test if a given url exists
-    function file_exists(url) {
-        // http://kevin.vanzonneveld.net
-        // +   original by: Enrique Gonzalez
-        // +      input by: Jani Hartikainen
-        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-        // %        note 1: This function uses XmlHttpRequest and cannot retrieve resource from different domain.
-        // %        note 1: Synchronous so may lock up browser, mainly here for study purposes. 
-        // *     example 1: file_exists('http://kevin.vanzonneveld.net/pj_test_supportfile_1.htm');
-        // *     returns 1: '123'
-        var req = this.window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
-        if (!req) {
-            throw new Error('XMLHttpRequest not supported');
-        }
-
-        // HEAD Results are usually shorter (faster) than GET
-        req.open('HEAD', url, false);
-        req.send(null);
-        if (req.status == 200) {
-            return true;
-        }
-
-        return false;
-    }
 
     // preg_replace
     // Code from : http://xuxu.fr/2006/05/20/preg-replace-javascript/
@@ -558,11 +558,11 @@
 
     function getContextMenuOptions(elem) {
         var optionsID = "";
-        
-        if(elem.attr('class') != undefined) {
+
+        if (elem.attr('class') != undefined) {
             optionsID = elem.attr('class').replace(/ /g, '_');
         }
-        
+
         if (optionsID == "") return 'itemOptions';
         if (!($('#' + optionsID).length)) {
             // Create a clone to itemOptions with menus specific to this element
@@ -860,7 +860,7 @@
             var centerHeight = (screen.height - windowHeight) / 2;
 
             // open popup
-            window.open("plugins/image-editor/index.html?path=" + data['Path'],
+            window.open("plugins/image-editor/index.html?path=" + data['Path'] + "&langCode=" + $.urlParam('langCode'),
                             "ImageEditor", "resizable=1, scrollbars=1, width=" + windowWidth + ", height=" + windowHeight + ",left=" + centerWidth + "top=" + centerHeight);
         } else {
             $.prompt(lg.edit_image_only);
